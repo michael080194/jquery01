c5e7d2ad07655abf0dd6094bb032a9bafd1fceb0
kyc080194 <michael080194@gmail.com>
2018-08-10 08:07:07 +0800

    php 資料 by GET 傳參數

--- test_ajax1.php
+++ test_ajax1.php
@@ -1,6 +1,11 @@
 <?php
+  $title = "";
+  if($_GET['title']){
+    $title = $_GET['title'];
+  }
+
   if($_GET['url'] == "kyc"){
-    echo "冠宇資訊有限公司";
+    echo "冠宇資訊有限公司" . $title;
   } else {
     echo "沒有傳網址";
   }
