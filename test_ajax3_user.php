78772aaf2c2c0fb41f74a8314c76a4eef8ca2c3e
kyc080194 <michael080194@gmail.com>
2018-08-12 09:13:19 +0800

    php 資料 by POST 傳參數 for serialize

--- test_ajax3_user.php
+++ test_ajax3_user.php
@@ -1,6 +1,11 @@
 <?php
-$user =isset($_POST['user']) ? $_POST['user'] : '';
-$email =isset($_POST['email']) ? $_POST['email'] : '';
-$sex =isset($_POST['sex']) ? $_POST['sex'] : '';
-echo $user."--".$email."--".$sex;
-?>
+$user  = isset($_POST['user']) ? $_POST['user'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$sex   = isset($_POST['sex']) ? $_POST['sex'] : '';
+
+$j = 10; // 100000000; test for time out or loading
+for ($i = 1; $i <= $j; $i++) {
+
+}
+
+echo $user . "--" . $email . "--" . $sex;
