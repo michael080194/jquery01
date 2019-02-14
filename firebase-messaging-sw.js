d0396aff17dbae0d1649140a9886020c9272a333
kyc080194 <michael080194@gmail.com>
2019-02-14 16:25:49 +0800

    j802_firebase_msg_test.html

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+var config = {
+ apiKey: "AIzaSyDS_C3TZgxS-blH2Q1QjnGdTX41198yw1U",
+ authDomain: "repair-b974d.firebaseapp.com",
+ databaseURL: "https://repair-b974d.firebaseio.com",
+ projectId: "repair-b974d",
+ storageBucket: "repair-b974d.appspot.com",
+ messagingSenderId: "626157948636"
+};
+
+firebase.initializeApp(config);
+// step--02
+// Retrieve Firebase Messaging object.
+const messaging = firebase.messaging();
