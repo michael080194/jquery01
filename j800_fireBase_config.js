1ffc4b77d2c598ee6e7d63693745e6b2231395a4
kyc080194 <michael080194@gmail.com>
2019-02-17 22:45:55 +0800

    firebase 連線設定值

---
+++ j800_fireBase_config.js
@@ -0,0 +1,11 @@
+var config = {
+ apiKey: "AIzaSyDS_C3TZgxS-blH2Q1QjnGdTX41198yw1U",
+ authDomain: "repair-b974d.firebaseapp.com",
+ databaseURL: "https://repair-b974d.firebaseio.com",
+ projectId: "repair-b974d",
+ storageBucket: "repair-b974d.appspot.com",
+ messagingSenderId: "626157948636"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
