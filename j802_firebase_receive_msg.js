b3a4664e4a39a8f029c67f6ed28b1a909c4caa88
kyc080194 <michael080194@gmail.com>
2019-02-19 14:41:58 +0800

    2018

--- j802_firebase_receive_msg.js
+++ j802_firebase_receive_msg.js
@@ -70,11 +70,12 @@
       console.log('Notification permission granted.');
       if('serviceWorker' in navigator) {
         console.log("serviceWorker  is supportted")
-        navigator.serviceWorker.register("firebase-messaging-sw.js").then(function() { //Include the service worker js file
-          //Registration worked
-        }).catch(function() {
-          //Registration didn't work
-        });        
+        // navigator.serviceWorker.register("firebase-messaging-sw.js").then(function() { //Include the service worker js file
+        //   //Registration worked
+        // }).catch(function() {
+        //   //Registration didn't work
+        // }); 
+        messaging.useServiceWorker('sw.js');               
         // firebase.messaging().useServiceWorker('firebase-messaging-sw.js')           
      }      
       if (!isTokenSentToServer()) {
